752899e74d7cc924078b41e785f60056a51a9afe
Aishalo <opeybabe_8@yahoo.com>
2022-10-13 21:07:33 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio>
+
+/**
+ * main - Entry point
+ * This file prints a test
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char prog_puzz[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(prog_puzz);
+
+	return (0);
+
+}
