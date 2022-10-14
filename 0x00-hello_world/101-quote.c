0c2289fa7e29c16a95158b2bda46b6de024776ac
Aishalo <opeybabe_8@yahoo.com>
2022-10-14 12:28:44 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - The body
+ * We use a different type of header
+ * Return: 1 for reasons yet discovered
+ */
+
+int main(vooid)
+{
+	char err[] = "and that piece ofo art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, err, 59);
+
+	return (1);
+
+}
