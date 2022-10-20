3dfeba2f2ba17fcb27518cbb7248a79dec723585
Aishalo <opeybabe_8@yahoo.com>
2022-10-19 17:48:39 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the english alphabet froom a-z.
+ * Return: nothing
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	foor (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
