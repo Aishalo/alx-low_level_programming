a17e66c2fd73d511c74109a34fbff3f1efef2502
Aishalo <opeybabe_8@yahoo.com>
2022-11-02 19:23:59 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,28 +1,24 @@
 #include "main.h"
 
 /**
- *  * factorial - returns the factorial of a given number.
- *
- *   * @n: input number.
- *
- *    * Return: factorial of the number.
- *
+ *factorial - returns the factorial of a given number.
+ *@n: input number.
+ *if: factorial of the number.
  */
-
 int factorial(int n)
 
 {
 
 	if (n < 0)
 
-					return (-1);
+	return (-1);
 
-			else if (n == 0)
+	else if (n == 0)
 
-						return (1);
+return (1);
 
-			else
+else
 
-							return (n * factorial(n - 1));
+	return (n * factorial(n - 1));
 
 }
