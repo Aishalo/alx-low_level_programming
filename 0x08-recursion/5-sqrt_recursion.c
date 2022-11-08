1c8abab8592aae1695c0cc952457cbf069ba7ab3
Aishalo <opeybabe_8@yahoo.com>
2022-11-08 02:10:47 -0800

    refresh

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,60 +1,18 @@
 #include "main.h"
 
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
 
 /**
- * is_prime - detects if an input number is a prime number.
- * @n: input number.
- * @c: iterator.
- *Return: 1 if n is a prime number. 0 if n is not a prime number.
+ * find_sqrt - finds the natural square root of an inputted number.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested
+ *
+ * Return: If the number has natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
  */
-
-int power_operation(int n, int c)
-
-   {
-
-if (c % (n / c) == 0)
-
-   {
-
-if (c * (n / c) == n)
-
-return (c);
-
-else
-
-return (-1);
-
-   }
-
-return (0 + power_operation(n, c + 1));
-
-   }
-
-/**
- * * is_prime_number - detects if an input number is a prime number.
- *
- *   * @n: input number.
- *
- *    * Return: 1 if n is a prime number. 0 if n is not a prime number.
- *
- */
-
-int is_prime_number(int n)
-
+int find_sqrt(int num, int root)
 {
-
-	if (n == 0)
-
-	return (0);
-
-	if (n < 0)
-
-	return (0);
-
-if (n == 1)
-
-	return (0);
-
-return (is_prime(n, 2));
-
-}
+                  if ((root * root) == num)
+	          return (root);	
+} 
